56e219283c82bae8124b671dc4cf0c58a48862b0
rtduany <rtduany@gmail.com>
2015-10-21 18:40:00 +0300

    add str function to models.py in myquery directory

--- questions/myquery/models.py
+++ questions/myquery/models.py
@@ -6,7 +6,13 @@ class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+	def __str__(self):
+		return self.question_text
+
 class Answer(models.Model):
 	question = models.ForeignKey(Question)
 	answer_text = models.CharField(max_length=200)
 	date_pub = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.answer_text
