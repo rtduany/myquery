44c513fdbdc6268f55623b64357121b84823dadb
rtduany <rtduany@gmail.com>
2015-10-21 17:52:14 +0300

    define the Answer class in our models.py file

--- questions/myquery/models.py
+++ questions/myquery/models.py
@@ -6,3 +6,7 @@ class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+class Answer(models.Model):
+	question = models.ForeignKey(Question)
+	answer_text = models.CharField(max_length=200)
+	date_pub = models.DateTimeField('date published')
